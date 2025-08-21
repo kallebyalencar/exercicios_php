f6117fd3575c288c434e2764814e040756e81d7b
Johnny kalleby Sobreira de Alencar <alencar.jkalleby@gmail.com>
2025-08-21 17:34:03 -0300

    funções para strings

--- desafios.php
+++ desafios.php
@@ -1,16 +1,85 @@
 <?php
 
-    $valor = 7;
-    $soma = 0;
+class ContaBancaria {
 
-    echo "Tabuada do 7:<br>";
+    private $titular;
+    private $numeroConta;
+    private $tipo;
+    private $saldo;
+    private $depositos;
 
-    for ($i = 1; $i <= 10; $i++){
-        $resultado = $i * $valor;
-        echo "$i x 7 = " . $resultado . "<br>";
-        $soma += $resultado;
+    public function __construct($titular, $numeroConta, $tipo, $saldoInicial = 0){
+        $this->titular = $titular;
+        $this->numeroConta = $numeroConta;
+        $this->tipo = $tipo;
+        $this->saldo = $saldoInicial;
+        $this->depositos = 0; 
     }
 
-    echo "A soma de todos os resultados da tabela: $soma";
+    public function getSaldo(){
+        return $this->saldo;
+    }
+
+    public function getDepositos(){
+        return $this->depositos;
+    }
+
+    public function getTitular(){
+        return $this->titular;
+    }
+
+    public function getNumeroConta(){
+        return $this->numeroConta;
+    }
+
+    public function depositar($valor){
+        if ($valor > 0) {
+            $this->saldo += $valor;
+            $this->depositos += $valor;
+        }
+    }
+
+    public function sacar($valor){
+        if ($valor > 0 && $valor <= $this->saldo) {
+            $this->saldo -= $valor;
+        }
+    }
+
+    public function transferir($valor, $contaDestino){
+        if ($valor > 0 && $valor <= $this->saldo) {
+            $this->saldo -= $valor;
+            $contaDestino->depositar($valor);
+        }
+    }
+
+    public function extrato(){
+        echo "Titular: {$this->titular}<br>";
+        echo "Conta: {$this->numeroConta}<br>";
+        echo "Tipo: {$this->tipo}<br>";
+        echo "Saldo atual: R$ " . number_format($this->saldo, 2, ',', '.') . "<br>";
+        echo "Total de depósitos: R$ " . number_format($this->depositos, 2, ',', '.') . "<br><br>";
+    }
+}
+
+$conta1 = new ContaBancaria("João", "12345-6", "Corrente", 0);
+$conta2 = new ContaBancaria("Kalleby", "22222-6", "Poupança", 0.99);
+
+$conta1->depositar(10000);
+$conta1->depositar(10000);
+$conta1->depositar(10000);
+$conta1->depositar(10000);
+$conta1->depositar(10000);
+$conta1->depositar(10000);
+$conta2->sacar(0);
+$conta1->transferir(5000, $conta2);
+
+
+echo "Extrato da Conta 1:<br>";
+$conta1->extrato();
+
+echo "----------<br><br>";
+
+echo "Extrato da Conta 2:<br>";
+$conta2->extrato();
 
 ?>
---
+++ praticas.php
@@ -0,0 +1,7 @@
+<?php
+
+$objeto = "Mouse";
+$novoObjeto = str_pad($objeto , 7, "*", STR_PAD_LEFT);
+echo $novoObjeto;
+
+?>
